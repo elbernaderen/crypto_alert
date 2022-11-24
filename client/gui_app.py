1ba3810987e71e903bbcb3f4bfd9c5a11af3b3b4
elbernaderen <65098903+elbernaderen@users.noreply.github.com>
2022-11-24 09:34:08 -0300

    added client folder with interface

--- client/gui_app.py
+++ client/gui_app.py
@@ -26,8 +26,37 @@ class Frame(tk.Frame):
         self.label_valor.grid(row = 1,column = 0, padx=10, pady=10)
         self.label_valor.config()
 
+        self.label_rsi = tk.Label(self,text ="RSI: ")
+        self.label_rsi.grid(row = 2,column = 0, padx=10, pady=10)
+        self.label_rsi.config()
+
+        self.label_macd = tk.Label(self,text ="MACD: ")
+        self.label_macd.grid(row = 3,column = 0, padx=10, pady=10)
+        self.label_macd.config()
+
+        self.label_media = tk.Label(self,text ="Media: ")
+        self.label_media.grid(row = 4,column = 0, padx=10, pady=10)
+        self.label_media.config()
+
+
         self.entry_simbolo = tk.Entry(self)
         self.entry_simbolo.grid(row = 0,column = 1, padx=10, pady=10)
 
         self.entry_valor = tk.Entry(self)
         self.entry_valor.grid(row = 1,column = 1, padx=10, pady=10)
+
+        self.entry_rsi = tk.Entry(self)
+        self.entry_rsi.grid(row = 2,column = 1, padx=10, pady=10)
+
+        self.entry_macd = tk.Entry(self)
+        self.entry_macd.grid(row = 3,column = 1, padx=10, pady=10)
+
+        self.entry_media = tk.Entry(self)
+        self.entry_media.grid(row = 4,column = 1, padx=10, pady=10)
+        self.entry_media_limite = tk.Entry(self)
+        self.entry_media_limite.grid(row = 4,column = 3, padx=10, pady=10)
+
+        # Botones
+        self.boton_inicio = tk.Button(self, text = "Iniciar")
+        self.boton_inicio.config(width = 20)
+        self.boton_inicio.grid(row = 6,column = 1, padx=10, pady=10)
