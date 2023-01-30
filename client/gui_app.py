23729b0f4b3f11cda34f741ab50eff4b9a5d0bba
elbernaderen <65098903+elbernaderen@users.noreply.github.com>
2023-01-30 10:08:21 -0300

    going to change the while loop

--- client/gui_app.py
+++ client/gui_app.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from model.mensaje import Valores_alerta,enviar_datos
+from model.mensaje import Valores_alerta,enviar_datos,salida
 
 def barra_menu(root):
     barra_menu = tk.Menu(root)
@@ -22,68 +22,66 @@ class Frame(tk.Frame):
         self.label_simbolo.grid(row = 0,column = 0, padx=10, pady=10)
         self.label_simbolo.config()
 
-
-        
-        
         self.label_valor = tk.Label(self,text ="Valor: ")
         self.label_valor.grid(row = 1,column = 1, padx=10, pady=10)
         self.label_valor.config()
-
 
         self.label_rsi = tk.Label(self,text ="RSI: ")
         self.label_rsi.grid(row = 2,column = 1, padx=10, pady=10)
         self.label_rsi.config()
 
-
-
         self.label_macd = tk.Label(self,text ="MACD: ")
         self.label_macd.grid(row = 3,column = 1, padx=10, pady=10)
         self.label_macd.config()
-
 
         self.mi_simbolo = tk.StringVar()
         self.entry_simbolo = tk.Entry(self,textvariable = self.mi_simbolo)
         self.entry_simbolo.grid(row = 0,column = 1, padx=10, pady=10)
 
-        options_list = ["<",">"]
+        options_list = ["<",">="]
 
-        value_valor = tk.StringVar()
-        value_valor.set("Select an Option")
-    
-        self.option_valor = tk.OptionMenu(self, value_valor, *options_list)
+        self.value_valor = tk.StringVar()
+        self.value_valor.set("Select an Option")
+     
+        self.option_valor = tk.OptionMenu(self, self.value_valor, *options_list)
         self.option_valor.grid(row = 1,column = 0, padx=10, pady=10)
 
         self.mi_valor = tk.StringVar()
         self.entry_valor = tk.Entry(self,textvariable = self.mi_valor)
         self.entry_valor.grid(row = 1,column = 2, padx=10, pady=10)
 
-        value_rsi = tk.StringVar()
-        value_rsi.set("Select an Option")
+        self.value_rsi = tk.StringVar()
+        self.value_rsi.set("Select an Option")
 
-        self.option_rsi = tk.OptionMenu(self, value_rsi, *options_list)
+        self.option_rsi = tk.OptionMenu(self, self.value_rsi, *options_list)
         self.option_rsi.grid(row = 2,column = 0, padx=10, pady=10)
+  
 
         self.mi_rsi = tk.StringVar()
         self.entry_rsi = tk.Entry(self,textvariable = self.mi_rsi)
         self.entry_rsi.grid(row = 2,column = 2, padx=10, pady=10)
 
-        value_macd = tk.StringVar()
-        value_macd.set("Select an Option")
+        self.value_macd = tk.StringVar()
+        self.value_macd.set("Select an Option")
 
-        self.option_macd = tk.OptionMenu(self, value_macd, *options_list)
+        self.option_macd = tk.OptionMenu(self, self.value_macd, *options_list)
         self.option_macd.grid(row = 3,column = 0, padx=10, pady=10)
 
-        
+
         self.mi_macd = tk.StringVar()
         self.entry_macd = tk.Entry(self,textvariable = self.mi_macd)
         self.entry_macd.grid(row = 3,column = 2, padx=10, pady=10)
 
-
-        # Boton
+        # Boton Inicio
     
         self.boton_inicio = tk.Button(self, text = "Iniciar",command = self.iniciar)
         self.boton_inicio.config(width = 20)
         self.boton_inicio.grid(row = 6,column = 1, padx=10, pady=10)
+        
+        # Boton Detener
+        self.boton_detener = tk.Button(self, text = "Detener",command = self.detener)
+        self.boton_detener.config(width = 20)
+        self.boton_detener.grid(row = 6,column = 2, padx=10, pady=10)
 
     def iniciar(self):
         dato = Valores_alerta(
@@ -91,11 +89,16 @@ class Frame(tk.Frame):
         self.mi_valor.get(),
         self.mi_rsi.get(),
         self.mi_macd.get(),
-        self.option_valor,
-        self.option_rsi,
-        self.option_macd
+        self.value_valor.get(),
+        self.value_rsi.get(),
+        self.value_macd.get()
         )
+        global running
+        running = True
 
         enviar_datos(dato)
 
+    def detener(self):
+        print("detener")
+        salida()
 
--- model/mensaje.py
+++ model/mensaje.py
@@ -1,16 +1,27 @@
-from messenger_d import mensajero
+from messenger_d import mensajero, salida
 
 class Valores_alerta:
     def __init__(self,simbolo,valor,rsi,macd,value_valor,value_rsi,value_macd):
         self.simbolo = simbolo
-        self.valor = float(valor)
-        self.rsi = float(rsi)
-        self.macd = float(macd)
-        self.value_valor = value_valor
-        self.value_rsi = value_rsi
-        self.value_macd = value_macd
+        self.valor = [valor,value_valor]
+        self.rsi = [rsi,value_rsi]
+        self.macd = [macd,value_macd]
+
+    def elemento(self,valores_real):
+        self.valor.append(valores_real.valor)
+        self.rsi.append(valores_real.rsi)
+        self.macd.append(valores_real.macd)
+
+        return self.valor,self.rsi, self.macd
+
+    def quita_elemento(self):
+        self.valor.pop()
+        self.rsi.pop()
+        self.macd.pop()
 
 def enviar_datos(alert):
     alert = alert
-    print(alert.macd)
     mensajero(alert)
+
+def salida():
+    salida()
